320fabd8e270b922758d36d57080d0bb8a51c68d
tohar <tohar65@gmail.com>
2025-06-23 10:43:17 +0300

    fix: specify port for web server to enhance configurability

--- web_app/web_server.py
+++ web_app/web_server.py
@@ -54,4 +54,5 @@ def all_alerts():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
